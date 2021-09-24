ca8e614e785123316caf3fbc5993210b07a9b41e
Paras Gherwada <parasgherwada41@gmail.com>
2021-09-24 12:27:33 +0530

    -

---
+++ find_lucky_integer_in_an_array.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+		int max_ele = *max_element(arr.begin(), arr.end());
+        int size  = max_ele + 1;
+		vector<int> lucky(size, 0);
+
+// 		cout << endl << "Size : " << size;
+
+        for(int val : arr)
+            ++lucky[val];
+
+        --size;
+        while(size > 0){
+// 			cout << endl << "Size : " << size << "\t lucky[size] : " << lucky[size];
+            if(size == lucky[size])
+                break;
+            --size;
+        }
+
+// 		cout << endl << endl;
+        return (size == 0) ? -1 : size;
+    }
+};
+
+int main(int args_c, char* args_v[]) {
+	vector<int> data = {
+// 		2, 2, 3, 4
+// 		1,2,2,3,3,3
+// 		2,2,2,3,3
+// 		5
+// 		7,7,7,7,7,7,7
+		4,3,2,2,4,1,3,4,3
+	};
+
+	Solution s;
+	cout << endl << "Lucky number : " << s.findLucky(data) << endl;
+	return 0;
+}
