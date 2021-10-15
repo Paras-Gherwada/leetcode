03cb92d694a12818dee2320b94239caf4e851054
Paras Gherwada <parasgherwada41@gmail.com>
2021-10-15 18:15:45 +0530

    -

--- find_common_character.cpp
+++ find_common_character.cpp
@@ -9,21 +9,12 @@ public:
 		vector<int> char_counter(26, 9999999);
 		for(string word : words) {
 			vector<int> temp_counter(26, 0);
-			for(char c : word)
-				++temp_counter[c - 'a'];
-
-			for(int idx = 0; idx < 26; ++idx) {
-				if(temp_counter[idx] < char_counter[idx])
-					char_counter[idx] = temp_counter[idx];
-			}
+			for(char c : word) ++temp_counter[c - 'a'];
+			for(int idx = 0; idx < 26; ++idx) if(temp_counter[idx] < char_counter[idx]) char_counter[idx] = temp_counter[idx];
 		}
 
 		vector<string> result;
-		for(int idx = 0; idx < 26; ++idx) {
-			for(int val = char_counter[idx]; val > 0; --val) {
-				result.push_back(string(1, 'a' + idx));
-			}
-		}
+		for(int idx = 0; idx < 26; ++idx) for(int val = char_counter[idx]; val > 0; --val) result.push_back(string(1, 'a' + idx));
 
         return result;
 	}
@@ -31,9 +22,9 @@ public:
 
 int main(int args_c, char* args_v[]) {
 	vector<string> input = {
-// 		"bella", "label", "roller"
-		"cool","lock","cook"
-// 		"acabcddd","bcbdbcbd","baddbadb","cbdddcac","aacbcccd","ccccddda","cababaab","addcaccd"
+		// "bella", "label", "roller"
+		// "cool","lock","cook"
+		// "acabcddd","bcbdbcbd","baddbadb","cbdddcac","aacbcccd","ccccddda","cababaab","addcaccd"
 	};
 
 	Solution s;
